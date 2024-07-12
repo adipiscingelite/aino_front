b6d5209a1166cf4809f529da3560322757242f7c
adipiscingelite <syaifulrahmatdani.18>
2024-07-12 09:12:29 +0700

    sedikit tambahan

--- src/app/dashboard/dashboard-components/top-cards/top-cards-data.ts
+++ src/app/dashboard/dashboard-components/top-cards/top-cards-data.ts
@@ -1,41 +1,38 @@
 export interface topcard {
-    bgcolor: string,
-    icon: string,
-    title: string,
-    href: string,
+    bgcolor: string;
+    icon: string;
+    title: string;
+    href: string;
     subtitle: string | number;
 }
 
 export const topcards: topcard[] = [
-
-  
     {
         bgcolor: 'success',
         icon: 'bi bi-file',
         title: 'Form DA',
-        href: 'http://localhost:4200/main/component/form-da',
+        href: '/main/component/form-da',
         subtitle: 0,
     },
     {
         bgcolor: 'danger',
         icon: 'bi bi-coin',
         title: 'Form ITCM',
-        href: 'http://localhost:4200/main/component/form-itcm',
+        href: '/main/component/form-itcm',
         subtitle: 0,
     },
     {
         bgcolor: 'warning',
         icon: 'bi bi-basket3',
         title: 'BA ITCM',
-        href: 'http://localhost:4200/main/component/ba-itcm',
+        href: '/main/component/ba-itcm',
         subtitle: 0,
     },
     {
         bgcolor: 'info',
         icon: 'bi bi-bag',
         title: 'Hak Akses',
-        href: '#',
-        subtitle: ''
+        href: '/main/component/hak-akses',
+        subtitle: '',
     },
-
-] 
+];
